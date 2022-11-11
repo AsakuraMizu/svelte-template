a6af473ff1c74f6c699c6a545c591aa24ecc7e56
AsakuraMizu <0xWATERx0@gmail.com>
2022-11-11 11:18:18 +0800

    update deps

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,15 @@
 </script>
 
 <main class="flex min-h-screen min-w-[320px] flex-col">
-  <div class="mx-0 my-auto max-w-7xl p-8 text-center">
+  <div class="mx-0 my-auto p-8 text-center">
     <div>
-      <a href="https://vitejs.dev" target="_blank">
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
         <img src="/vite.svg" class="logo" alt="Vite Logo" />
       </a>
-      <a href="https://svelte.dev" target="_blank">
+      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
         <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
       </a>
-      <a href="https://svelte.dev" target="_blank">
+      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
         <img src={twLogo} class="logo tw" alt="Tailwind CSS Logo" />
       </a>
     </div>
@@ -24,8 +24,9 @@
     </div>
 
     <p>
-      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the
-      official Svelte app framework powered by Vite!
+      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">
+        SvelteKit
+      </a>, the official Svelte app framework powered by Vite!
     </p>
 
     <p class="text-[#888]">Click on the logos to learn more</p>
@@ -39,9 +40,11 @@
     padding: 1.5em;
     will-change: filter;
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
+
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
