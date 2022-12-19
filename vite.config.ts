07c8973afe44c4bfd73f0732d4323d1c775954bd
AsakuraMizu <0xWATERx0@gmail.com>
2022-12-20 00:09:18 +0800

    migrate to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-import unocss from 'unocss/vite';
+import unocss from '@unocss/vite';
 import { extractorSvelte } from '@unocss/core';
-import {
-  presetUno,
-  presetAttributify,
-  presetIcons,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss';
+import presetUno from '@unocss/preset-uno';
+import presetAttributify from '@unocss/preset-attributify';
+import presetIcons from '@unocss/preset-icons';
+import transformerDirectives from '@unocss/transformer-directives';
+import transformerVariantGroup from '@unocss/transformer-variant-group';
 
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
