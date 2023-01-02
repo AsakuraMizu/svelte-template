35cece3afaa6be2fbecfa197eb198078b3cc8585
AsakuraMizu <0xWATERx0@gmail.com>
2023-01-02 21:17:42 +0800

    seperate unocss config

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 import unocss from '@unocss/vite';
-import { extractorSvelte } from '@unocss/core';
-import presetUno from '@unocss/preset-uno';
-import presetAttributify from '@unocss/preset-attributify';
-import presetIcons from '@unocss/preset-icons';
-import transformerDirectives from '@unocss/transformer-directives';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
-
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    unocss({
-      extractors: [extractorSvelte],
-      presets: [presetUno({ dark: 'media' }), presetAttributify(), presetIcons()],
-      transformers: [transformerDirectives(), transformerVariantGroup()],
-    }),
+    unocss(),
     svelte({
       preprocess: [vitePreprocess()],
       hot: !process.env.VITEST,
